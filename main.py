15bb683be642358a58253d6d93437f9869d39910
Steve Gossip <stephengossip@gmail.com>
2022-02-06 21:39:57 +0000

    able to access all the li tags that will link to each distilleries range of whiskys.

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ req = Request(whisky_notes_reviews_url, headers={'User-Agent': 'Mozilla/5.0'})
 webpage = urlopen(req).read()
 
 page_soup = soup(webpage, 'html.parser')
-print(page_soup.prettify())
+
+distillery_links = page_soup.find_all(name='li', class_='cat-item')
+
+print(len(distillery_links))
+print(distillery_links[0].text)
+print(distillery_links[-1].text)
 
