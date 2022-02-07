96368ea5dfbe1498838a3885f3fd245964e7bbb8
Steve Gossip <stephengossip@gmail.com>
2022-02-07 21:18:26 +0000

    I have isolated the hrefs for each of the scottish whisky distilleries in a list.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from bs4 import BeautifulSoup as soup
 from urllib.request import Request, urlopen
+from selenium import webdriver
+import time
 
 whisky_notes_reviews_url = 'https://www.whiskynotes.be/whisky-reviews-by-distillery/'
+# CHROME_DRIVER_PATH = '/Users/admin/Development/chromedriver'
+# driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
+
 
 req = Request(whisky_notes_reviews_url, headers={'User-Agent': 'Mozilla/5.0'})
 
@@ -9,9 +14,16 @@ webpage = urlopen(req).read()
 
 page_soup = soup(webpage, 'html.parser')
 
-distillery_links = page_soup.find_all(name='li', class_='cat-item')
+distilleries = page_soup.find_all(name='li', class_='cat-item')
 
-print(len(distillery_links))
-print(distillery_links[0].text)
-print(distillery_links[-1].text)
+distillery_links = [distillery.find('a')['href'] for distillery in distilleries]
+scottish_distillery_links = distillery_links[13:]
+
+print(scottish_distillery_links[0])
+print(scottish_distillery_links[-1])
+
+# driver.get(whisky_notes_reviews_url)
+# time.sleep(10)
+
+
 
